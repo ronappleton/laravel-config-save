a65cc837d50e02e28171775f5d8224f2ca7792f6
Ron Appleton <ron.appleton@snappyshopper.co.uk>
2022-01-06 17:15:41 +0000

    Add base files

--- src/Facades/ConfigSaver.php
+++ src/Facades/ConfigSaver.php
@@ -2,7 +2,16 @@
 
 namespace Ronappleton\LaravelConfigSave\Facades;
 
-class ConfigSaver
-{
+use Illuminate\Support\Facades\Facade;
+use Ronappleton\LaravelConfigSave\Writers\ConfigWriter;
 
+/**
+ * @method static store()
+ */
+class ConfigSaver extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return ConfigWriter::class;
+    }
 }
--- src/Writers/ConfigWriter.php
+++ src/Writers/ConfigWriter.php
@@ -1,8 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Ronappleton\LaravelConfigSave\Writers;
+
+use JsonException;
+use Symfony\Component\Finder\Finder;
 
 class ConfigWriter
 {
+    private Finder $finder;
 
+    private array $configs;
+
+    /**
+     * @throws JsonException
+     */
+    public function __construct(Finder $finder)
+    {
+        $this->finder = $finder;
+        $this->configs = json_decode(
+            file_get_contents(dirname(getcwd()) . '/Storage/configs.json'),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
+        );
+    }
+
+    public function store()
+    {
+        dd($this->configs, $this->getFiles());
+    }
+
+    private function getFiles()
+    {
+        $configFiles = $this->finder->path(config_path())->files()->name('*.php');
+    }
 }
